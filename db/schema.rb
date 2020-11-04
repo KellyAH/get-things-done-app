392a8791eb227f5e231f990984365ae329bac34a
Kelly Hong <k_ahong@yahoo.com>
2020-11-04 15:53:09 -0800

    save point

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_24_193303) do
+ActiveRecord::Schema.define(version: 2020_10_28_231843) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "description"
+    t.integer "priority"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "thoughts", force: :cascade do |t|
     t.string "description"
