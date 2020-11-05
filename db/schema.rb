158f67cbadbc76041f5047a7a7d99c1ef7fc4fa2
nirmala <nirmala@surf.net.np>
2020-11-05 17:29:29 +0100

    Fix foreign key in schema

--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_11_03_195159) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_user_categories_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
