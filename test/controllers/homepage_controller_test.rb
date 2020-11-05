af4ceaa7620c8f00a66aaa55141cba544624b5b7
nirmala <nirmala@surf.net.np>
2020-11-05 23:04:48 +0100

    Remove rails buit-in test folder

--- test/controllers/homepage_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class HomepageControllerTest < ActionDispatch::IntegrationTest
-  test "should get greeting" do
-    get homepage_greeting_url
-    assert_response :success
-  end
-
-end
