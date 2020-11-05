448400f38c151456246955c1153104e98a29fff1
nirmala <nirmala@surf.net.np>
2020-11-05 16:31:01 +0100

    Remove errored migration and create direct relationship inside table

--- db/migrate/20201103195159_create_user_categories.rb
+++ db/migrate/20201103195159_create_user_categories.rb
@@ -1,7 +1,7 @@
 class CreateUserCategories < ActiveRecord::Migration[6.0]
   def change
     create_table :user_categories do |t|
-      t.integer :user_id
+      t.belongs_to :user
       t.string :name
 
       t.timestamps
