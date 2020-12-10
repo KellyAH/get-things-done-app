01e53ed9511a273ffd1253c66ecc9e85ecfccbc6
nirmala <nirmala@surf.net.np>
2020-12-11 00:29:56 +0100

    Remove comments and change route to thoughts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   resources :thoughts
-  root to: 'homepage#index'
+  root to: 'thoughts#index'
 
-  get "/thoughts/:id", to: "thoughts#show"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
