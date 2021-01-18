33339ddd7803b8f26ef4891e47bc1e54335eef4f
Kelly Hong <k_ahong@yahoo.com>
2021-01-18 12:53:38 -0800

    create model unit tests for thoughts and tasks

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
+require 'minitest/autorun'
 
 class TaskTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+end
+
+class TaskModelTest < Minitest::Test
+  def test_task_created_when_required_data_exists
+    record_created = Task.new(description: 'blahfoobar', priority: 'low').save
+    assert(record_created == true)
+    fetch_created_record = Task.where(description: 'blahfoobar', priority: 'low').exists?
+    assert(fetch_created_record == true)
+  end
+
+  def test_tasks_not_created_when_missing_priorty
+    record_created = Task.new(description: 'blah').save
+    assert(record_created == false)
+  end
+
+  def test_tasks_not_created_when_missing_description
+    record_created = Task.new(priority: 'low').save
+    assert(record_created == false)
+  end
+
+  def test_tasks_not_created_when_missing_required_data
+    record_created = Task.new().save
+    assert(record_created == false)
+  end
 end
--- test/models/thought_test.rb
+++ test/models/thought_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
+require 'minitest/autorun'
 
 class ThoughtTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+end
+
+class ThoughtModelTest < Minitest::Test
+  def test_thought_created_when_required_data_exists
+    record_created = Thought.new(description: 'blah').save
+    assert(record_created == true)
+    fetch_created_record = Thought.where(description: 'blah').exists?
+    assert(fetch_created_record == true)
+  end
+
+  # To simulate a breaking app change is caught by this test:
+  # 1. go to app/models/thought.rb
+  # 2. comment out validates_presence_of :description
+  def test_thought_not_created_when_missing_required_data
+    record_created = Thought.new().save
+    assert(record_created == false)
+  end
 end
