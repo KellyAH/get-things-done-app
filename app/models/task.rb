3ad6c6c39c95224d9753111b835747927e4f42cd
Kelly Hong <k_ahong@yahoo.com>
2021-01-23 12:09:35 -0800

    add codecov to get test coverage

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,8 @@
 class Task < ApplicationRecord
   enum priority: { high: "high", medium: "medium", low: "low" }
   # ensures a Task record cannot be created if its missing description and priority
-  validates_presence_of :description, :priority
+  # validates_presence_of :description, :priority
+  validates :description, presence: true
+  validates :priority, presence: true
 end
+
--- app/models/user_category.rb
+++ app/models/user_category.rb
@@ -1,5 +1,4 @@
 class UserCategory < ApplicationRecord
-  belongs_to :user, :dependent => :destroy
+  # belongs_to :user, :dependent => :destroy
+  belongs_to :user, dependent: :destroy
 end
-
-
