58b327d06386968444757f299b0fd9ac66baf8db
Kelly Hong <k_ahong@yahoo.com>
2021-01-30 12:31:07 -0800

    add fixture data and refactor unit tests

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -2,32 +2,23 @@ require 'test_helper'
 require 'minitest/autorun'
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
-
-class TaskModelTest < Minitest::Test
-  # Testing Task.new and not Task.create because Task.new is used the TasksController.
-  def test_task_created_when_required_data_exists
-    record_created = Task.new(description: 'blahfoobar', priority: 'low').save
-    assert(record_created == true)
-    fetch_created_record = Task.where(description: 'blahfoobar', priority: 'low').exists?
-    assert(fetch_created_record == true)
+  test "test_task_created_when_required_data_exists" do
+    assert Task.create(description: 'blahfoobar', priority: 'low')
   end
 
-  def test_tasks_not_created_when_missing_priorty
-    record_created = Task.new(description: 'blah').save
-    assert(record_created == false)
+  test "test_tasks_not_created_when_missing_priority" do
+    task = Task.new(priority: nil)
+    refute task.save
+
+    assert task.errors.include?(:priority)
+    assert task.errors.details[:priority].include?(error: :blank)
   end
 
-  def test_tasks_not_created_when_missing_description
-    record_created = Task.new(priority: 'low').save
-    assert(record_created == false)
-  end
+  test "test_tasks_not_created_when_missing_description" do
+    task = Task.new(description: nil)
+    refute task.save
 
-  def test_tasks_not_created_when_missing_required_data
-    record_created = Task.new().save
-    assert(record_created == false)
+    assert task.errors.include?(:description)
+    assert task.errors.details[:description].include?(error: :blank)
   end
 end
